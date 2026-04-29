b5b17494a2265e82e2a2e1271d99af5c4c728a60
Aliayman38 <aliayalquraan8@gmail.com>
2026-04-29 22:17:25 +0300

    Ali: Added tqdm progress bar for video processing

--- src/team_clustering/clusterer.py
+++ src/team_clustering/clusterer.py
@@ -7,17 +7,18 @@ class TeamClusterer:
         self.team_colors = {}
 
     def get_player_color(self, frame, bbox):
-        x1, y1, x2, y2 = map(int, bbox)
-        player_img = frame[y1:y2, x1:x2]
-        
-        shirt_zone = player_img[0:int(player_img.shape[0]*0.4), :]
-        
-        if shirt_zone.size == 0: return np.array([0,0,0])
-
-        pixels = shirt_zone.reshape(-1, 3)
-        kmeans = KMeans(n_clusters=1, n_init=1)
-        kmeans.fit(pixels)
-        return kmeans.cluster_centers_[0]
+     x1, y1, x2, y2 = map(int, bbox)
+     player_img = frame[y1:y2, x1:x2]
+    
+     height, width, _ = player_img.shape
+     shirt_img = player_img[int(height*0.2):int(height*0.5), int(width*0.2):int(width*0.8)]
+    
+     if shirt_img.size == 0: return np.array([0,0,0])
+ 
+     hsv_img = cv2.cvtColor(shirt_img, cv2.COLOR_BGR2HSV)
+     
+     avg_color = np.mean(hsv_img, axis=(0, 1))
+     return avg_color
 
     def assign_teams(self, frame, player_bboxes):
         player_colors = []
